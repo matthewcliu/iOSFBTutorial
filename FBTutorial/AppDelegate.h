064ddb146e40ac1c04a5e1dccadf37d3963946a7
Matthew Liu <matthewliu@gmail.com>
2013-06-11 16:10:21 -0700

    Initial Commit

---
+++ FBTutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FBTutorial
+//
+//  Created by Matthew Liu on 6/11/13.
+//  Copyright (c) 2013 Matthew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FBTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBTutorial
+//
+//  Created by Matthew Liu on 6/11/13.
+//  Copyright (c) 2013 Matthew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
